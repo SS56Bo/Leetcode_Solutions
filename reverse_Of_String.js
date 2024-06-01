24849188e0dea7aef078c8dc540374cd7f65906f
SS56Bo <sohamsarkar560@gmail.com>
2024-06-01 12:31:33 +0530

    DSA Problem

---
+++ reverse_Of_String.js
@@ -0,0 +1,21 @@
+// Write a program to reverse a string.
+// Example:
+//      Input: Somebody Else
+//      Output: eslE ydobemoS
+
+const reverse = (str) => {
+  //check input
+  if (!str || str.length < 2 || typeof str !== "string") {
+    return "Invalid input!";
+  } else {
+  }
+  const backwards = [];
+  const totalItems = str.length - 1;
+  for (let i = totalItems; i >= 0; i--) {
+    backwards.push(str[i]);
+  }
+  //console.log(backwards);
+  return backwards.join("");
+};
+
+console.log(reverse("Somebody Else"));
