d2cc68b029454f6bb8e4f478b67ec7a44d283c56
SS56Bo <sohamsarkar560@gmail.com>
2024-06-01 12:59:11 +0530

    DSA Problem

--- merged_Sorted_arrays.js
+++ merged_Sorted_arrays.js
@@ -3,6 +3,12 @@ const mergeArrays = (array1, array2) => {
   const array1Item = array1[0];
   const array2Item = array2[0];
   let i = 0;
+  if (array1.length === 0) {
+    return array2;
+  }
+  if (array2.length === 0) {
+    return array1;
+  }
 };
 
 test1 = mergeArrays([1, 2, 3], []);
